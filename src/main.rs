52fb845f4a1987931edc3f51ca79b53ece033c3c
HiraokaTakuya <hiraoka64@gmail.com>
2022-08-04 00:26:03 +0900

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use itertools::Itertools;
+use rand::prelude::*;
+
+struct Dodosuko(ThreadRng);
+
+impl Dodosuko {
+    const DATA: [&'static str; 2] = ["ドド", "スコ"];
+}
+
+impl Iterator for Dodosuko {
+    type Item = &'static str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Self::DATA.choose(&mut self.0).map(|x| {
+            print!("{}", x);
+            *x
+        })
+    }
+}
+
+fn main() {
+    Dodosuko(rand::thread_rng())
+        .into_iter()
+        .tuple_windows()
+        .take_while(|item| {
+            !matches!(
+                item,
+                (
+                    "ドド", "スコ", "スコ", "スコ", "ドド", "スコ", "スコ", "スコ", "ドド", "スコ", "スコ", "スコ",
+                )
+            )
+        })
+        .for_each(|_| {});
+    println!("ラブ注入♡");
+}
